4c24bbd7f5995d98a03a81f2da6d1c550381c0c1
Srinivasulu_DevOps <srinivasulubehara4@gmail.com>
2026-08-23 02:41:51 +0530

    Update UserController.java

--- backend/src/main/java/com/example/employee_service_mama/controller/UserController.java
+++ backend/src/main/java/com/example/employee_service_mama/controller/UserController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 @CrossOrigin(
         origins = {
-                "https://teamhub.in",
+                "https://srinivasulutech.shop",
                 "http://teamhub.in",
                 "http://teamhub-ALB-680655485.ap-south-1.elb.amazonaws.com",
                 "http://15.206.26.130:80",
