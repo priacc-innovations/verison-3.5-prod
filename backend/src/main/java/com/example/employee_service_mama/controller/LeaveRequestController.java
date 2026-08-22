0d32b9d47678f1e49827100299de8a3d59cc6af1
Srinivasulu_DevOps <srinivasulubehara4@gmail.com>
2026-08-23 02:41:14 +0530

    Update LeaveRequestController.java

--- backend/src/main/java/com/example/employee_service_mama/controller/LeaveRequestController.java
+++ backend/src/main/java/com/example/employee_service_mama/controller/LeaveRequestController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @RequestMapping("/api/leave")
 @CrossOrigin(
         origins = {
-                "https://teamhub.in",
+                "https://srinivasulutech.shop",
                 "http://teamhub.in",
                 "http://teamhub-ALB-680655485.ap-south-1.elb.amazonaws.com",
                 "http://15.206.26.130:80"
