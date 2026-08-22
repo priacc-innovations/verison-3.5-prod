9a4150e1f6e8399f048093068a2549341fe8bf08
Srinivasulu_DevOps <srinivasulubehara4@gmail.com>
2026-08-23 02:39:57 +0530

    Update AttendanceController.java

--- backend/src/main/java/com/example/employee_service_mama/controller/AttendanceController.java
+++ backend/src/main/java/com/example/employee_service_mama/controller/AttendanceController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @RequestMapping("/api/attendance")
 @CrossOrigin(
         origins = {
-                "https://teamhub.in",
+                "https://srinivasulutech.shop",
                 "http://teamhub.in",
                 "http://teamhub-ALB-680655485.ap-south-1.elb.amazonaws.com",
                 "http://15.206.26.130:80"
